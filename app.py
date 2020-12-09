7a5f6d9f04bc8c0cfb8faffa2448b36dad26f254
dev_mrahsansaleem <ahsansaleem6389@gmail.com>
2020-12-09 23:05:58 +0500

    new functionality to imageupload end point

--- app.py
+++ app.py
@@ -42,42 +42,72 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def get_response_image(image_path):
     with open(image_path, "rb") as img_file:
         encoded_Image = b64encode(img_file.read())
-
-    # pil_img = Image.open(image_path, mode='r')
-    # byte_arr = io.BytesIO()
-    # pil_img.save(byte_arr, format='PNG')
-    # encoded_Image = encodebytes(byte_arr.getvalue()).decode('base64')
     return encoded_Image.decode("utf-8")
 
 
+def Resize(img):
+    # image = np.asarray(img)
+    resized_Img = cv2.resize(img, (208, 176), interpolation=cv2.INTER_LINEAR)
+    return resized_Img
+
+
+def Normalization(img):
+    norm_img = np.zeros((800, 800))
+    normalize_img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+    return normalize_img
+
+
 def extraction(Brain_Image):
-    Brain_Image = np.asarray(Brain_Image)
+    # final_image = plt.imshow(Brain_Image, cmap="gray")
+    # final_image = plt.show()
     # print(type(Brain_Image))
+    Brain_Image = np.asarray(Brain_Image)
     gray = cv2.cvtColor(Brain_Image, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
     ret, markers = cv2.connectedComponents(thresh)
+    #Get the area taken by each component. Ignore label 0 since this is the background.
     marker_area = [
         np.sum(markers == m) for m in range(np.max(markers)) if m != 0
     ]
-    largest_component = np.argmax(marker_area) + 1
+    #Get label of largest component by area
+    largest_component = np.argmax(
+        marker_area) + 1  #Add 1 since we dropped zero above
+    #Get pixels which correspond to the brain
     brain_mask = markers == largest_component
     brain_out = Brain_Image.copy()
+    #In a copy of the original image, clear those pixels that don't correspond to the brain
     brain_out[brain_mask == False] = (0, 0, 0)
     return brain_out
 
 
-def mainFun(inputImagePath):
+def Enhancement(en_img):
+    #en_img=np.asarray(en_img)
+    ret, enhancedImage = cv2.threshold(en_img, 130, 255, cv2.THRESH_TOZERO)
+    return enhancedImage
 
+
+def imageProcessing(inputImagePath, inputFileName):
     img_brain = cv2.imread(inputImagePath)
-    height = 208
-    width = 176
-    dim = (width, height)
-    # res_img = []
-    res = cv2.resize(img_brain, dim, interpolation=cv2.INTER_LINEAR)
-    # img = stringToImage(base64_URL)
-    brain_extracted_img = extraction(res)
-    # brain_extracted_base64_String = imageToString(brain_extracted_img)
-    return brain_extracted_img
+    Resized_image = Resize(img_brain)
+    Normalized_image = Normalization(Resized_image)
+    brain_extracted_img = extraction(Normalized_image)
+    enhanced_brain_img = Enhancement(brain_extracted_img)
+
+    # extrated saving and decodeing
+    brain_extracted_img_File = Image.fromarray(brain_extracted_img)
+    brain_extracted_img_Path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                            "extraction_" + inputFileName)
+    brain_extracted_img_File.save(brain_extracted_img_Path)
+    encoded_extration_image = get_response_image(brain_extracted_img_Path)
+
+    # enhanced saving and decodeing
+    enhanced_brain_img_File = Image.fromarray(enhanced_brain_img)
+    brain_enhanced_img_Path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                           "enhanced_" + inputFileName)
+    enhanced_brain_img_File.save(brain_enhanced_img_Path)
+    encoded_enhanced_image = get_response_image(brain_enhanced_img_Path)
+
+    return encoded_extration_image, encoded_enhanced_image
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -106,24 +136,17 @@ def upload_file():
             if not isdir(app.config['UPLOAD_FOLDER']):
                 mkdir(app.config['UPLOAD_FOLDER'])
 
-            temp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(temp)
-            # print(temp)
+            inputImagePath = os.path.join(app.config['UPLOAD_FOLDER'],
+                                          filename)
+            file.save(inputImagePath)
 
-            data = mainFun(temp)
-
-            data = Image.fromarray(data)
-            temp = os.path.join(app.config['UPLOAD_FOLDER'],
-                                "extraction_" + filename)
-            data.save(temp)
-            encoded_extration_image = get_response_image(temp)
-            # print(data)
-            # ShowImage('Connected Components', data, 'rgb')
+            encoded_extration_image, encoded_enhanced_image = imageProcessing(
+                inputImagePath, filename)
 
             return {
                 "status": "success",
-                "path": temp,
-                "encoded_extration_image": encoded_extration_image
+                "encoded_extration_image": encoded_extration_image,
+                "encoded_enhanced_image": encoded_enhanced_image
             }, 201
     else:
         return {"status": "request not found"}, 404
