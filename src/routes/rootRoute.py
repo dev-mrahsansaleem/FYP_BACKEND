39fd1db068663ab1377780a546d5a14f79b2ad9d
dev-mrahsansaleem <ahsansaleem6389@gmail.com>
2021-02-02 23:48:33 +0500

    add testTokken route

--- src/routes/rootRoute.py
+++ src/routes/rootRoute.py
@@ -1,9 +1,18 @@
 from src import app
+from src.helper.functions import token_required
 
 from flask_cors.decorator import cross_origin
+from flask import jsonify
 
 
 @app.route('/')
 @cross_origin()
 def webPage():
     return "working"
+
+
+@app.route('/api/testTokken')
+@cross_origin()
+@token_required
+def testTokken():
+    return jsonify({"status": "tokken is valid"})
